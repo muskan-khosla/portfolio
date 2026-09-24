2aca8388b83bb1873d68827bc8b95974fe7fa7ff
Muskan Khosla <muskankhosla.dev@gmail.com>
2026-09-24 06:47:24 +0000

    Sep 24, 2026, 12:17 PM

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+=========================================================
+PORTFOLIO WEBSITE
+Muskan Khosla
+=========================================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Portfolio website loaded.");
+
+});
